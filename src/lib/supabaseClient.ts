ea4f45632ddd53a0e8a2f788486e157d278e7867
clarkemedina <s0ulninja@yahoo.com>
2026-02-17 03:09:09 -0800

    add supabase anon key

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL!,
-  import.meta.env.VITE_SUPABASE_ANON_KEY!
+  "https://cirbccredjxmyzpfvvil.supabase.co",
+  "sb_publishable_cqw11BoaV8msmQLlBkUNjw_akHAc_NO"
 );
