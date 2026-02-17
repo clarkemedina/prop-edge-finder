5da88c7cb66df3c5e1b5dd925d82145b9496ecda
clarkemedina <s0ulninja@yahoo.com>
2026-02-17 03:18:42 -0800

    BASE

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  "https://cirbccredjxmyzpfvvil.supabase.co",
-  "sb_publishable_cqw11BoaV8msmQLlBkUNjw_akHAc_NO"
+  import.meta.env.VITE_SUPABASE_URL!,
+  import.meta.env.VITE_SUPABASE_ANON_KEY!
 );
